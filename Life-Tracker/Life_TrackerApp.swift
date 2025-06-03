6f4e47ec7cf33261b533bdbe66d65e5ee64ce14e
DuduBertolDev <88258208+DuduBertol@users.noreply.github.com>
2025-06-03 08:06:13 -0300

    Initial Commit - Create xcode Project

---
+++ Life-Tracker/Life_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_TrackerApp.swift
+//  Life-Tracker
+//
+//  Created by Eduardo Bertol on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Life_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
